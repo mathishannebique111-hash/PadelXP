2be960dd611142396f6844f4e66ed2a11b8a8668
mathishannebique111-hash <mathis.hannebique111@gmail.com>
2026-02-06 12:42:09 +0100

    fix(ui): update loader icon to authentic lucide-style tennis ball

--- components/ui/PadelLoader.tsx
+++ components/ui/PadelLoader.tsx
@@ -26,9 +26,8 @@ export default function PadelLoader({ text, className = "" }: PadelLoaderProps) 
           strokeLinejoin="round"
         >
           <circle cx="12" cy="12" r="10" />
-          <path d="M2 12h20" />
-          <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10" />
-          <path d="M12 2a15.3 15.3 0 0 0-4 10 15.3 15.3 0 0 0 4 10" />
+          <path d="M2 12a10 10 0 0 0 10 10" />
+          <path d="M12 2a10 10 0 0 1 10 10" />
         </svg>
       </div>
       {text && (
